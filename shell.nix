3f7f16c33d5d81eeea1902a4a9485c308f57253c
Karolis Stasaitis <contact@carlossless.io>
2023-05-07 22:58:19 +0200

    nix fixup

--- shell.nix
+++ shell.nix
@@ -1,16 +1,21 @@
 { pkgs ? import <nixpkgs> { } }:
 with pkgs;
 mkShell {
-  buildInputs = lib.flatten [
-    [rustup]
-    (lib.optionals (stdenv.hostPlatform.isDarwin) [
-      darwin.apple_sdk.frameworks.IOKit
-      darwin.apple_sdk.frameworks.AppKit
-      iconv
-    ])
-  ];
+  buildInputs = [
+    rustc
+    cargo
+  ] ++
+  (lib.optionals (stdenv.hostPlatform.isLinux) [
+    udev
+    pkg-config
+  ]) ++
+  (lib.optionals (stdenv.hostPlatform.isDarwin) [
+    darwin.apple_sdk.frameworks.IOKit
+    darwin.apple_sdk.frameworks.AppKit
+    iconv
+  ]);
 
   shellHook = ''
-    echo "Entered the dev shell! $(rustup --version) $(cargo --version)"
+    echo "Entered the dev shell! $(rustc --version) $(cargo --version)"
   '';
 }
