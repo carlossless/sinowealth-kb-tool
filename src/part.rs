0432e568f147dc97a12dbb9f8f0ae96f6c4b283c
Swift Geek (Sebastian Grzywna) <270845+swiftgeek@users.noreply.github.com>
2023-05-21 13:36:26 +0200

    part.rs: Add RE-K70-BYK800 board (#1)
    Board uses BYK801 FW / SH68F881 MCU
    Tested with read / write operations, dumped contents match one obtained
    from ICP mode (JTAG pins).
    USB string descriptor was modified in the dump to provide a simple test case
    for write feature

--- src/part.rs
+++ src/part.rs
@@ -24,9 +24,18 @@ pub const PART_XINMENG_K916: Part = Part {
     product_id: 0x00a1,
 };
 
+pub const PART_RE_K70_BYK800: Part = Part {
+    flash_size: 28672, // 28672 until bootloader
+    bootloader_size: 4096,
+    page_size: 2048,
+    vendor_id: 0x258a,
+    product_id: 0x001a,
+};
+
 pub static PARTS: phf::Map<&'static str, Part> = phf_map! {
     "nuphy-air60" => PART_NUPHY_AIR60,
-    "xinmeng-k916" => PART_XINMENG_K916
+    "xinmeng-k916" => PART_XINMENG_K916,
+    "re-k70-byk800" => PART_RE_K70_BYK800,
 };
 
 impl Part {
