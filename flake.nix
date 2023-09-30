019c8741d01c47809760d946d876e0d38c0da663
Karolis Stasaitis <contact@carlossless.io>
2023-09-30 20:25:50 +0200

    distribution through nix flake (#11)
    Changes here enable easy distribution of this tool as a nix flake.
    
    Usage example:
    ```nix
    {
      inputs = {
        flake-utils.url = "github:numtide/flake-utils";
        sinowealth-kb-tool.url = "github:carlossless/sinowealth-kb-tool";
      };
    
      outputs = { self, nixpkgs, flake-utils, sinowealth-kb-tool }:
        flake-utils.lib.eachDefaultSystem (
          system: let pkgs = nixpkgs.legacyPackages.${system}; in
            {
              devShells.default = pkgs.mkShell {
                buildInputs = with pkgs; [
                  sinowealth-kb-tool.packages."${system}".default
                ];
              };
            }
        );
    }
    ```

--- flake.nix
+++ flake.nix
@@ -2,18 +2,69 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    naersk.url = "github:nix-community/naersk";
     rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
-  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
-    flake-utils.lib.eachDefaultSystem
-      (system:
-        let overlays = [ (import rust-overlay) ];
-            pkgs = import nixpkgs {
-              inherit system overlays;
-            }; in
-        {
-          devShells.default = import ./shell.nix { inherit pkgs; };
-        }
-      );
+  outputs = { self, nixpkgs, flake-utils, naersk, rust-overlay }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [ (import rust-overlay) ];
+
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+
+        toolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+
+        naersk' = pkgs.callPackage naersk {
+          cargo = toolchain;
+          rustc = toolchain;
+          clippy = toolchain;
+        };
+
+        buildInputs = with pkgs; [
+          pkg-config
+          libusb1
+        ] ++
+        (lib.optionals (stdenv.hostPlatform.isLinux) [
+          udev
+        ]) ++
+        (lib.optionals (stdenv.hostPlatform.isDarwin) [
+          darwin.apple_sdk.frameworks.IOKit
+          darwin.apple_sdk.frameworks.AppKit
+          iconv
+        ]);
+      in
+      {
+        formatter = pkgs.nixpkgs-fmt;
+
+        packages = {
+          # For `nix build` `nix run`, & `nix profile install`:
+          default = naersk'.buildPackage rec {
+            pname = "sinowealth-kb-tool";
+            version = "latest";
+
+            src = ./.;
+            doCheck = true; # run `cargo test` on build
+
+            inherit buildInputs;
+
+            meta = with pkgs.lib; {
+              description = "A utility for reading and writing flash contents on Sinowealth 8051-based devices";
+              homepage = "https://github.com/carlossless/sinowealth-kb-tool";
+              license = licenses.mit;
+              mainProgram = "sinowealth-kb-tool";
+              maintainers = with maintainers; [ carlossless ];
+            };
+          };
+        };
+
+        devShells.default = pkgs.mkShell {
+          inherit buildInputs;
+          nativeBuildInputs = with pkgs; [ rustup toolchain ];
+        };
+      }
+    );
 }
