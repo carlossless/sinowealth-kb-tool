9e9b476bdb5f8f31b7c4c29829022b259ded537f
Karolis Stasaitis <contact@carlossless.io>
2024-03-18 20:51:08 +0100

    device: redragon-k641-shaco-pro

--- src/part.rs
+++ src/part.rs
@@ -95,6 +95,12 @@ pub const PART_REDRAGON_ANIVIA_K614: Part = Part {
     ..PART_BASE_SH68F90
 };
 
+pub const PART_REDRAGON_K641_SHACO_PRO: Part = Part {
+    vendor_id: 0x258a,
+    product_id: 0x0049,
+    ..PART_BASE_SH68F90
+};
+
 pub const PART_GENESIS_THOR_300: Part = Part {
     vendor_id: 0x258a,
     product_id: 0x001f,
@@ -167,6 +173,7 @@ pub static PARTS: Map<&'static str, Part> = phf_map! {
     "re-k70-byk800" => PART_RE_K70_BYK800,
     "redragon-k614-anivia" => PART_REDRAGON_ANIVIA_K614,
     "redragon-k617-fizz" => PART_REDRAGON_FIZZ_K617,
+    "redragon-k641-shaco-pro" => PART_REDRAGON_K641_SHACO_PRO,
     "royalkludge-rk100" => PART_ROYALKLUDGE_RK100,
     "royalkludge-rk84-iso-return" => PART_ROYALKLUDGE_RK84_ISO_RETURN,
     "royalkludge-rk61" => PART_ROYALKLUDGE_RK61,
