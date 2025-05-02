4ccb938f78ef5de08f03be61f3a8772bec21dbd9
Karolis Stasaitis <contact@carlossless.io>
2025-05-02 12:51:39 +0200

    win fix

--- src/device_selector.rs
+++ src/device_selector.rs
@@ -382,7 +382,7 @@ impl DeviceSelector {
                     #[cfg(target_os = "windows")]
                     {
                         let (descriptor, feature_report_ids) = self.get_d_f(path);
-                        interface_node.children.push(ItemNode {
+                        children.push(ItemNode {
                             path: path.to_str().unwrap().to_string(),
                             usage_page: d.usage_page(),
                             usage: d.usage(),
